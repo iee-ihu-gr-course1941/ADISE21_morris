624b2020a63ab6bc914d7251697ec05c6731265b
Junny13 <lena.mparmpa@gmail.com>
2022-01-17 01:02:05 +0200

    typo

--- lib/game.php
+++ lib/game.php
@@ -20,7 +20,7 @@ function show_status() {
 function check_abort() {
 	global $mysqli;
 	
-	$sql = "update game_status set status='aborted', result=if(p_turn='W','B','W'),p_turn=null where p_turn is not null and last_change<(now()-INTERVAL 5 MINUTE) and status='started'";
+	$sql = "update game_status set status='aborded', result=if(p_turn='W','B','W'),p_turn=null where p_turn is not null and last_change<(now()-INTERVAL 5 MINUTE) and status='started'";
 	$st = $mysqli->prepare($sql);
 	$r = $st->execute();
 }
@@ -66,7 +66,7 @@ function update_status_delete($color, $val) {
 	$status = read_status();
 
 	if(($status['w_setup'] == 1 && $status['b_setup'] == 1) && $total_pieces < 3) {
-		$final_sql = "update game_status set status='aborted', result=?";
+		$final_sql = "update game_status set status='aborded', result=?";
 		$final_st=$mysqli->prepare($final_sql);
 		$final_st->bind_param('s',$p_turn);
 		$final_st->execute();
@@ -84,16 +84,16 @@ function update_game_status() {
 	$new_status=null;
 	$new_turn=null;
 	
-	$st3=$mysqli->prepare('select count(*) as aborted from players WHERE last_action< (NOW() - INTERVAL 5 MINUTE)');
+	$st3=$mysqli->prepare('select count(*) as aborded from players WHERE last_action< (NOW() - INTERVAL 5 MINUTE)');
 	$st3->execute();
 	$res3 = $st3->get_result();
-	$aborted = $res3->fetch_assoc()['aborted'];
-	if($aborted>0) {
+	$aborded = $res3->fetch_assoc()['aborded'];
+	if($aborded>0) {
 		$sql = "UPDATE players SET username=NULL, token=NULL WHERE last_action< (NOW() - INTERVAL 5 MINUTE)";
 		$st2 = $mysqli->prepare($sql);
 		$st2->execute();
 		if($status['status']=='started') {
-			$new_status='aborted';
+			$new_status='aborded';
 		}
 	}
 
